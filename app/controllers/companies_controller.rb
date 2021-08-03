5453679658af9b9e78ad3db7ad56deda534f107d
rahimshahad <rahim.shahad2@gmail.com>
2021-08-03 11:01:43 -0400

    implemented first signup

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,12 +1,24 @@
 class CompaniesController < ApplicationController
 
     def new 
+        @company = Company.new
     end
 
 
     def create 
+        @company = Company.create(comp_params)
+        redirect_to company_path(@company)
     end
 
     def show 
+        @company = Company.find_by_id(params[:id])
     end 
+
+
+
+    private
+    def comp_params
+        params.require(:company).permit(:name, :email, :password)
+    end
+
 end
